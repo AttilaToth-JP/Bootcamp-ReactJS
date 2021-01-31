fa0d632c9d6a0b9753e832edc1e9c1a3bdabbfb5
Attila <47952706+AttilaToth-JP@users.noreply.github.com>
2021-01-31 19:53:12 +0000

    Prectice result

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+ReactDOM.render(
+  <div>
+    <h1>Top three bands I loved as a teenager</h1>
+    <ul>
+      <li>Kispal es a Borz</li>
+      <li>The Cure</li>
+      <li>The Doors</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
