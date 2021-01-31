ddc3bf41564a9ca26f31983e83c2975430f35ec7
Attila <47952706+AttilaToth-JP@users.noreply.github.com>
2021-01-31 19:38:31 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(
-  <div>
-    <h1>Hello world!</h1>
-    <p>This is a paragraph</p>
-  </div>,
-  document.getElementById("root")
-);
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
