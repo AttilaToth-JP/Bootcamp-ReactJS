c61532477a1bf076d4d331077ccb5ff71d938663
Attila <47952706+AttilaToth-JP@users.noreply.github.com>
2021-02-01 20:09:50 +0000

    Lesson_3 _code_practice finished

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-//Create a react app from scratch.
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "T-bug";
+
+function currentYear() {
+  return new Date().getFullYear();
+}
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}.</p>
+    <p>Copyright {currentYear()}</p>
+  </div>,
+  document.getElementById("root")
+);
+
+//Angela's solution
+// const currentDate = new Date();
+// const year = currentDate.getFullYear();
