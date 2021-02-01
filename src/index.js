a5711538f0dc65ffc9a17c963475bc7a87ed9ec6
Attila <47952706+AttilaToth-JP@users.noreply.github.com>
2021-02-01 18:29:30 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <div>
-    <h1>Top three bands I loved as a teenager</h1>
-    <ul>
-      <li>Kispal es a Borz</li>
-      <li>The Cure</li>
-      <li>The Doors</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
