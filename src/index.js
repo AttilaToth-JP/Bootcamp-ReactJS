a1914581f3a650236dd6606d63c11590d2bb0309
Attila <47952706+AttilaToth-JP@users.noreply.github.com>
2021-02-01 19:37:33 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const fName = "Attila";
-const lName = "Toth";
-const luckyNr = 37;
-
-ReactDOM.render(
-  <div>
-    <h1>
-      Hello {`${fName} ${lName}`} / {fName + " " + lName}!
-    </h1>
-    <p>Your lucky number is {luckyNr}.</p>
-  </div>,
-  document.getElementById("root")
-);
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
