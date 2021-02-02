d3bfe9c018067af32bd61bd6456c57c0a6327b2f
Attila <47952706+AttilaToth-JP@users.noreply.github.com>
2021-02-02 20:27:10 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const fName = "Attila";
-const lName = "Toth";
-const luckyNr = 37;
+const img = "https://picsum.photos/300";
 
 ReactDOM.render(
   <div>
-    <h1>
-      Hello {`${fName} ${lName}`} / {fName + " " + lName}!
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      Top three bands I loved as a teenager
     </h1>
-    <p>Your lucky number is {luckyNr}.</p>
+    <div>
+      <img
+        className="band-img"
+        alt="The Cure"
+        src="https://www.rockarchive.com/media/3031/the-cure-05.jpg?width=1120&height=undefined&upscale=false&slimmage=true&center=0.5,0.5&mode=crop"
+      />
+      <img
+        className="band-img"
+        alt="Kispal es a Borz"
+        src="https://magyarnarancs.hu/data/articles/54/541/article-54153/200013_40.jpg"
+      />
+      <img
+        className="band-img"
+        alt="The Doors"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4oS56Z4AE_4FceiEekGq4y-NykDibwHGRCA&usqp=CAU"
+      />
+    </div>
+    <img alt="random image" src={img + "?grayscale"} />
   </div>,
   document.getElementById("root")
 );
