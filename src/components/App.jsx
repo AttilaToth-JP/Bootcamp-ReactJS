b94c6c5de1dc41c4c48d17efacc0d1a1e33b311f
Attila <47952706+AttilaToth-JP@users.noreply.github.com>
2021-02-09 18:58:23 +0000

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-import Heading from "./Heading";
 
 function App() {
-  return <Heading />;
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <div className="card">
+        <div className="top">
+          <h2>Beyonce</h2>
+          <img
+            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+            alt="avatar_img"
+          />
+        </div>
+        <div className="bottom">
+          <p>+123 456 789</p>
+          <p>b@beyonce.com</p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default App;
