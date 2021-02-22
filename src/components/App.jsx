ff0c696452db4c32cf01f17e9b79418ac2b1bb84
Attila <47952706+AttilaToth-JP@users.noreply.github.com>
2021-02-22 23:12:27 +0000

    Lesson 14 practice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import LoginForm from "./Login";
-
-var isLoggedIn = false;
 
 function App() {
   return (
     <div className="container">
-      {isLoggedIn ? <h1>Hello</h1> : <LoginForm />}
-      {/* AND operator 
-      //currentTime > 12 && <h1>Why are you still working</h1> */}
+      <h1>TIME</h1>
+      <button>Get Time</button>
     </div>
   );
 }
--- src/components/Input.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function input(props) {
-  return <input type={props.type} placeholder={props.placeholder} />;
-}
-
-export default input;
